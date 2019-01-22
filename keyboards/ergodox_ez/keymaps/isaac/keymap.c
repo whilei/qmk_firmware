566f68faf30282c84ad0fdf784af10873a83f961
ia <isaac.ardis@gmail.com>
2019-01-22 13:40:18 -0600

    9nWW

--- keyboards/ergodox_ez/keymaps/isaac/keymap.c
+++ keyboards/ergodox_ez/keymaps/isaac/keymap.c
@@ -24,19 +24,19 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   // Qwerty and friends
   [0] = LAYOUT_ergodox(
                        // Left
-                       TG(1) , KC_1 , KC_2 , KC_3 , KC_4 , KC_5 , KC_TRANSPARENT ,
-                       KC_CAPS , KC_Q , KC_W , KC_E , KC_R , KC_T , LCTL(KC_C) ,
-                       LT(2 , KC_ESCAPE) , ALT_T(KC_A) , KC_S , KC_D , KC_F , KC_G ,
-                       KC_LSPO , CTL_T( KC_Z ) , KC_X , KC_C , KC_V , KC_B , KC_ESCAPE,
+                       TG(1) , KC_1 , KC_2 , KC_3 , KC_4 , KC_5 , LCTL(KC_C) ,
+                       KC_CAPS , KC_Q , KC_W , KC_E , KC_R , KC_T , KC_ESCAPE ,
+                       LT(2 , KC_ESCAPE) , KC_A , KC_S , KC_D , KC_F , KC_G ,
+                       KC_LSPO , CTL_T( KC_Z ) , KC_X , KC_C , KC_V , KC_B , KC_BSPACE ,
                        CTL_T(KC_NO) , KC_TRANSPARENT , SCMD_T(KC_NO) , ALT_T(KC_NO) , KC_LGUI ,
                        KC_0 , KC_DLR ,
                        KC_RIGHT ,
                        KC_SPACE , LT(2 , KC_TAB) , KC_LEFT ,
                        // Right
-                       KC_TRANSPARENT , KC_6 , KC_7 , KC_8 , KC_9 , KC_0 , KC_TRANSPARENT ,
-                       LCTL(KC_V) , KC_Y , KC_U , KC_I , KC_O , KC_P , KC_BSPACE ,
+                       LCTL(KC_V) , KC_6 , KC_7 , KC_8 , KC_9 , KC_0 , KC_TRANSPARENT ,
+                       OSM(MOD_LSFT) , KC_Y , KC_U , KC_I , KC_O , KC_P , KC_BSPACE ,
                        KC_H , KC_J , KC_K , KC_L , LT(2 , KC_SCOLON) , KC_QUOTE ,
-                       KC_UP , LT(4, KC_N) , KC_M , KC_COMMA , KC_DOT , KC_SLASH , KC_RSPC ,
+                       KC_UP , KC_N , KC_M , KC_COMMA , KC_DOT , LT(4, KC_SLASH ) , KC_RSPC ,
                        CTL_T(KC_NO) , C_S_T(KC_NO) , KC_TRANSPARENT , KC_TRANSPARENT , KC_DOWN ,
                        KC_TRANSPARENT , KC_DELETE ,
                        KC_LALT ,
@@ -85,15 +85,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                        KC_TRANSPARENT , KC_SLASH ,
                        KC_TRANSPARENT ,
                        KC_TRANSPARENT , KC_COLN , KC_EQUAL
-                       /* // right */
-                       /* KC_TRANSPARENT , KC_TRANSPARENT , KC_TRANSPARENT , KC_TRANSPARENT , KC_TRANSPARENT , KC_TRANSPARENT , KC_TRANSPARENT , */
-                       /* KC_KP_SLASH , KC_KP_PLUS , KC_7 , KC_8 , KC_9 , KC_TRANSPARENT , KC_TRANSPARENT , */
-                       /* KC_AMPR , KC_4 , KC_5 , KC_6 , KC_TRANSPARENT , KC_TRANSPARENT , */
-                       /* KC_MINUS , KC_KP_ASTERISK , KC_1 , KC_2 , KC_3 , KC_TRANSPARENT , KC_TRANSPARENT , */
-                       /* KC_0 , KC_KP_DOT , KC_TRANSPARENT , KC_TRANSPARENT , KC_TRANSPARENT , */
-                       /* KC_TRANSPARENT , KC_TRANSPARENT , */
-                       /* KC_TRANSPARENT , */
-                       /* KC_TRANSPARENT , KC_COLN , KC_EQUAL */
                        ),
 
   // Dvorak
@@ -125,8 +116,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                        KC_TRANSPARENT , KC_TRANSPARENT , KC_TRANSPARENT , KC_TRANSPARENT , KC_TRANSPARENT , KC_TRANSPARENT , KC_TRANSPARENT ,
                        KC_TRANSPARENT , KC_TRANSPARENT , KC_MS_WH_LEFT , KC_MS_UP , KC_MS_WH_RIGHT , KC_MS_WH_UP , KC_TRANSPARENT ,
                        KC_TRANSPARENT , KC_TRANSPARENT , KC_MS_LEFT , KC_MS_DOWN , KC_MS_RIGHT , KC_MS_WH_DOWN ,
-                       /* KC_TRANSPARENT , KC_TRANSPARENT , KC_MS_WH_UP , KC_MS_UP , KC_MS_WH_DOWN , KC_TRANSPARENT , KC_TRANSPARENT , */
-                       /* KC_TRANSPARENT , KC_MS_WH_LEFT , KC_MS_LEFT , KC_MS_DOWN , KC_MS_RIGHT , KC_MS_WH_RIGHT , */
                        KC_TRANSPARENT , KC_TRANSPARENT , KC_TRANSPARENT , KC_TRANSPARENT , KC_TRANSPARENT , KC_TRANSPARENT , KC_TRANSPARENT ,
                        KC_TRANSPARENT , KC_TRANSPARENT , KC_TRANSPARENT , KC_TRANSPARENT , KC_TRANSPARENT ,
                        KC_TRANSPARENT , KC_TRANSPARENT ,
@@ -135,8 +124,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                        // right
                        KC_TRANSPARENT , KC_TRANSPARENT , KC_TRANSPARENT , KC_TRANSPARENT , KC_TRANSPARENT , KC_TRANSPARENT , KC_TRANSPARENT ,
                        KC_TRANSPARENT , KC_TRANSPARENT , KC_TRANSPARENT , KC_UP , KC_TRANSPARENT , KC_TRANSPARENT , KC_TRANSPARENT ,
-                       KC_TRANSPARENT , KC_LEFT , KC_DOWN , KC_RIGHT , KC_TRANSPARENT , KC_TRANSPARENT ,
-                       KC_TRANSPARENT , KC_TRANSPARENT , KC_TRANSPARENT , KC_TRANSPARENT , KC_TRANSPARENT , KC_TRANSPARENT , KC_TRANSPARENT ,
+                       KC_TRANSPARENT , KC_TRANSPARENT , KC_UP , KC_TRANSPARENT , KC_TRANSPARENT , KC_TRANSPARENT ,
+                       KC_TRANSPARENT , KC_LEFT , KC_DOWN , KC_RIGHT , KC_TRANSPARENT , KC_TRANSPARENT , KC_TRANSPARENT ,
                        KC_TRANSPARENT , KC_TRANSPARENT , KC_TRANSPARENT , KC_TRANSPARENT , KC_TRANSPARENT ,
                        KC_TRANSPARENT , KC_TRANSPARENT ,
                        KC_TRANSPARENT ,
