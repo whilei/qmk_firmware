73b442f98921f710502ae0cac49a77dff0b9a4fb
ia <isaac.ardis@gmail.com>
2019-01-27 19:14:08 -0500

    NORMAN to default

--- keyboards/ergodox_ez/keymaps/isaac/keymap.c
+++ keyboards/ergodox_ez/keymaps/isaac/keymap.c
@@ -21,44 +21,55 @@ enum custom_keycodes {
 // 3
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-  // Qwerty and friends
+
+  // Norman and friends
   [0] = LAYOUT_ergodox(
                        // Left
                        TG(1) , KC_1 , KC_2 , KC_3 , KC_4 , KC_5 , LCTL(KC_C) ,
-                       KC_TAB , KC_Q , KC_W , KC_E , KC_R , KC_T , KC_ESCAPE ,
-                       LT(2 , KC_ESCAPE) , KC_A , KC_S , KC_D , KC_F , KC_G ,
+
+                       KC_TAB , KC_Q , KC_W , KC_D , KC_F , KC_K , KC_ESCAPE ,
+                       LT(2 , KC_ESCAPE) , KC_A , KC_S , KC_E , KC_T , KC_G ,
                        KC_LSPO , CTL_T( KC_Z ) , KC_X , KC_C , KC_V , KC_B , KC_MINUS ,
+
                        CTL_T(KC_NO) , KC_TRANSPARENT , SCMD_T(KC_NO) , ALT_T(KC_NO) , KC_LGUI ,
                        KC_0 , KC_DLR ,
                        KC_RIGHT ,
                        KC_SPACE , KC_TAB , KC_LEFT ,
+
                        // Right
                        LCTL(KC_V) , KC_6 , KC_7 , KC_8 , KC_9 , KC_0 , TG(3) ,
-                       OSM(MOD_LSFT) , KC_Y , KC_U , KC_I , KC_O , KC_P , KC_BSPACE ,
-                       KC_H , KC_J , KC_K , KC_L , LT(2 , KC_SCOLON) , LT(4, KC_QUOTE ) ,
-                       KC_UP , KC_N , KC_M , KC_COMMA , KC_DOT , LT(3, KC_SLASH ) , KC_RSPC ,
+
+                       OSM(MOD_LSFT) , KC_J , KC_U , KC_R , KC_L , KC_SCOLON , KC_BSPACE ,
+                       KC_Y , KC_N , KC_I , KC_O , LT(2 , KC_H) , LT(4, KC_QUOTE ) ,
+                       KC_UP , KC_P , KC_M , KC_COMMA , KC_DOT , LT(3, KC_SLASH ) , KC_RSPC ,
+
                        CTL_T(KC_NO) , C_S_T(KC_NO) , KC_TRANSPARENT , KC_TRANSPARENT , KC_DOWN ,
                        KC_TRANSPARENT , KC_DELETE ,
                        KC_LALT ,
                        KC_LGUI , KC_BSPACE , KC_ENTER
                        ),
 
-  // Norman
+  // QWERTY
   [1] = LAYOUT_ergodox(
                        // Left
                        KC_TRANSPARENT , KC_TRANSPARENT , KC_TRANSPARENT , KC_TRANSPARENT , KC_TRANSPARENT , KC_TRANSPARENT , KC_TRANSPARENT ,
-                       KC_TRANSPARENT , KC_Q , KC_W , KC_D , KC_F , KC_K , KC_TRANSPARENT ,
-                       KC_TRANSPARENT , KC_A , KC_S , KC_E , KC_T , KC_G ,
+
+                       KC_TRANSPARENT , KC_Q , KC_W , KC_E , KC_R , KC_T , KC_TRANSPARENT ,
+                       KC_TRANSPARENT , KC_A , KC_S , KC_D , KC_F , KC_G ,
                        KC_TRANSPARENT , KC_Z , KC_X , KC_C , KC_V , KC_B , KC_TRANSPARENT ,
+
                        KC_TRANSPARENT , KC_TRANSPARENT , KC_TRANSPARENT , KC_TRANSPARENT , KC_TRANSPARENT ,
                        KC_TRANSPARENT , KC_TRANSPARENT ,
                        KC_TRANSPARENT ,
                        KC_TRANSPARENT , KC_TRANSPARENT , KC_TRANSPARENT ,
+
                        // Right
                        KC_TRANSPARENT , KC_TRANSPARENT , KC_TRANSPARENT , KC_TRANSPARENT , KC_TRANSPARENT , KC_TRANSPARENT , KC_TRANSPARENT ,
-                       KC_TRANSPARENT , KC_J , KC_U , KC_R , KC_L , KC_SCOLON , KC_TRANSPARENT ,
-                       KC_Y , KC_N , KC_I , KC_O , LT(2, KC_H ) , KC_TRANSPARENT ,
-                       KC_TRANSPARENT , KC_P , KC_M , KC_COMMA , KC_DOT , LT(4, KC_SLASH ) , KC_TRANSPARENT ,
+
+                       KC_TRANSPARENT , KC_Y , KC_U , KC_I , KC_O , KC_P , KC_TRANSPARENT ,
+                       KC_H , KC_J , KC_K , KC_L , LT(2, KC_SCOLON ) , KC_TRANSPARENT ,
+                       KC_TRANSPARENT , KC_N , KC_M , KC_COMMA , KC_DOT , LT(3, KC_SLASH ) , KC_TRANSPARENT ,
+
                        KC_TRANSPARENT , KC_TRANSPARENT , KC_TRANSPARENT , KC_TRANSPARENT , KC_TRANSPARENT ,
                        KC_TRANSPARENT , KC_TRANSPARENT ,
                        KC_TRANSPARENT ,
