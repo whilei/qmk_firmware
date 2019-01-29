f93e753bb00f92eb1057663a9fdeed1c043381b4
ia <isaac.ardis@gmail.com>
2019-01-29 07:03:09 -0600

    use easier togglers for vim bindings

--- keyboards/ergodox_ez/keymaps/isaac/keymap.c
+++ keyboards/ergodox_ez/keymaps/isaac/keymap.c
@@ -25,19 +25,20 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   // Norman and friends
   [0] = LAYOUT_ergodox(
                        // Left
-                       TG(1) , KC_1 , KC_2 , KC_3 , KC_4 , KC_5 , LCTL(KC_C) ,
+                       TG(1) , KC_1 , KC_2 , KC_3 , KC_4 , KC_5 , KC_TRANSPARENT ,
 
                        KC_TAB , KC_Q , KC_W , KC_D , KC_F , KC_K , KC_ESCAPE ,
                        LT(2 , KC_ESCAPE) , KC_A , KC_S , KC_E , KC_T , KC_G ,
-                       KC_LSPO , CTL_T( KC_Z ) , KC_X , KC_C , KC_V , KC_B , KC_MINUS ,
+                       KC_LSPO , CTL_T( KC_Z ) , LT(2, KC_X) , KC_C , KC_V , KC_B , TG(4) ,
 
                        CTL_T(KC_NO) , KC_TRANSPARENT , SCMD_T(KC_NO) , ALT_T(KC_NO) , KC_LGUI ,
                        KC_0 , KC_DLR ,
                        KC_RIGHT ,
-                       KC_SPACE , LT(1, KC_TAB ), KC_LEFT ,
+                       // this is nice for vim bindings
+                       LT(1, KC_SPACE) , LT(4, KC_TAB ), KC_LEFT ,
 
                        // Right
-                       LCTL(KC_V) , KC_6 , KC_7 , KC_8 , KC_9 , KC_0 , TG(3) ,
+                       KC_TRANSPARENT , KC_6 , KC_7 , KC_8 , KC_9 , KC_0 , TG(3) ,
 
                        OSM(MOD_LSFT) , KC_J , KC_U , KC_R , KC_L , KC_SCOLON , KC_BSPACE ,
                        KC_Y , KC_N , KC_I , KC_O , LT(2 , KC_H) , LT(4, KC_QUOTE ) ,
@@ -46,7 +47,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                        CTL_T(KC_NO) , C_S_T(KC_NO) , KC_TRANSPARENT , KC_TRANSPARENT , KC_DOWN ,
                        KC_TRANSPARENT , KC_DELETE ,
                        KC_LALT ,
-                       KC_LGUI , KC_BSPACE , KC_ENTER
+                       KC_LGUI , KC_BSPACE , LT(1, KC_ENTER)
                        ),
 
   // QWERTY
@@ -68,7 +69,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
                        KC_TRANSPARENT , KC_Y , KC_U , KC_I , KC_O , KC_P , KC_TRANSPARENT ,
                        KC_H , KC_J , KC_K , KC_L , LT(2, KC_SCOLON ) , KC_TRANSPARENT ,
-                       KC_TRANSPARENT , KC_N , KC_M , KC_COMMA , KC_DOT , LT(3, KC_SLASH ) , KC_TRANSPARENT ,
+                       KC_TRANSPARENT , KC_N , KC_M , KC_TRANSPARENT , KC_TRANSPARENT , KC_TRANSPARENT , KC_TRANSPARENT ,
 
                        KC_TRANSPARENT , KC_TRANSPARENT , KC_TRANSPARENT , KC_TRANSPARENT , KC_TRANSPARENT ,
                        KC_TRANSPARENT , KC_TRANSPARENT ,
