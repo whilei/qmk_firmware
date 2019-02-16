8f5d87368bcc90590eaf36a44a2fb36505fbf190
ia <isaac.ardis@gmail.com>
2019-02-16 05:49:41 -0600

    :floppy_disk:

--- keyboards/ergodox_ez/keymaps/isaac/keymap.c
+++ keyboards/ergodox_ez/keymaps/isaac/keymap.c
@@ -88,7 +88,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                        KC_Y , KC_N , KC_I , KC_O , KC_H , MO(SYMBOLS) , // b/c i use symbols a lot and just can't afford to wait 200ms till they kick in
                        KC_BSPACE , KC_P , KC_M , KC_COMMA , KC_DOT , LT(MOTION, KC_SLASH ) , KC_RSPC ,
 
-                       CTL_T(KC_NO) , ___ , LALT(KC_B) , LALT(KC_F) , KC_DOWN ,
+                       CTL_T(KC_NO) , ALT_T(KC_NO) , ___ , ___ , KC_DOWN ,
 
                        LGUI(KC_H) , LGUI(KC_L) ,
                        LCS(KC_V) ,
@@ -126,9 +126,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [SYMBOLS] = LAYOUT_ergodox(
                        // Left
                        ___ , ___ , ___ , ___ , ___ , ___ , ___ ,
-                       ___ , KC_PERC , LSFT( KC_QUOTE ) , KC_GRAVE , KC_TILD , ___ , ___ ,
-                       ___ , KC_AT , KC_DLR , KC_LCBR , KC_RCBR , ___ ,
-                       ___ , KC_BSLASH , KC_CIRC , KC_LBRACKET, KC_RBRACKET,  ___ , ___ ,
+                       ___ , KC_PERC , KC_QUOTE , KC_GRAVE , KC_TILD , ___ , ___ ,
+                       ___ , KC_AT , KC_DLR , KC_LCBR , KC_RCBR , KC_KP_PLUS ,
+                       ___ , KC_BSLASH , KC_CIRC , KC_LBRACKET, KC_RBRACKET,  KC_HASH , ___ ,
                        ___ , ___ , ___ , ___ , ___ ,
                        ___ , ___ ,
                        ___ ,
@@ -138,7 +138,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                        /* ___ , KC_QUOTE , KC_TILD , KC_MINUS , KC_GRAVE , ___ , ___ , */
                              /* KC_HASH, LSFT(KC_QUOTE) , KC_PIPE , KC_AMPR , ___ , ___ , */
                        /* ___ , KC_KP_ASTERISK , KC_UNDS , KC_LABK , KC_RABK , ___ , ___ , */
-                       ___ , ___ , KC_EXLM , KC_MINUS , KC_QUOTE , ___ , ___ ,
+                       ___ , ___ , KC_EXLM , KC_MINUS , LSFT(KC_QUOTE) , ___ , ___ ,
                        KC_COLN, KC_EQUAL , KC_PIPE , KC_AMPR , ___ , ___ ,
                        ___ , KC_KP_ASTERISK , KC_UNDS , KC_LABK , KC_RABK , ___ , ___ ,
                                               ___ , ___ , ___ , ___ , ___ ,
@@ -159,7 +159,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [NUMPAD] = LAYOUT_ergodox(
                       // Left
                       ___ , KC_F1 , KC_F2 , KC_F3 , KC_F4 , KC_F5 , KC_F6 ,
-                      ___ , ___ , WR_ESCAPEDDOUBLEQUOTE , WR_CODEFENCE , ___ , ___ , ___ ,
+                      ___ , ___ , WR_ESCAPEDDOUBLEQUOTE , ___ , WR_CODEFENCE ,  ___ , ___ ,
                       ___ , WR_2AND1 , ___ , ___ , ___ , ___ ,
                       ___ , ___ , ___ , ___ , ___ , ___ , ___ ,
                       ___ , ___ , ___ , ___ , ___ ,
@@ -168,8 +168,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                       ___ , ___ , ___ ,
                       // right
                       KC_F7 , KC_F8 , KC_F9 , KC_F10 , KC_F11 , KC_F12 , ___ ,
-                      ___ , KC_KP_PLUS , KC_7 , KC_8 , KC_9 , KC_KP_ASTERISK , ___ ,
-                      KC_MINUS , KC_4 , KC_5 , KC_6 , KC_SLASH , WR_ESCAPEDRETURN ,
+                      ___ , KC_KP_PLUS , KC_7 , KC_8 , KC_9 , KC_SLASH , ___ ,
+                      KC_KP_ASTERISK , KC_4 , KC_5 , KC_6 , KC_MINUS , WR_ESCAPEDRETURN ,
                       ___ , KC_DOT , KC_1 , KC_2 , KC_3 , KC_0 , KC_ENTER ,
                       ___ , ___ , ___ , ___ , ___ ,
                       ___ , ___ ,
@@ -211,9 +211,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                       ___ , ___ , ___ ,
                       // right
                       ___ , ___ , ___ , ___ , ___ , ___ , TG(XPLANE) ,
-                      TMUX_PSPLITV , ___ , KC_7 , KC_8 , KC_9 , ___ , TMUX_PSPLITH ,
-                      ___ , KC_4 , KC_5 , KC_6 , ___ , TMUX_COPYMODE ,
-                      TMUX_PFS , TMUX_PANE_SELECT , KC_1 , KC_2 , KC_3 , KC_0 , TMUX_PASTE ,
+                      TMUX_PSPLITV , ___ , ___ , ___ , ___ , ___ , TMUX_PSPLITH ,
+                      ___ , ___ , ___ , ___ , ___ , ___ ,
+                      TMUX_PFS , ___ , ___ , ___ , ___ , ___ , ___ ,
                       TMUX_WCREATE , ___ , ___ , TMUX_WKILL , DOEXIT ,
                       TMUX_WP , TMUX_WN ,
                       TMUX_PLAST ,
@@ -457,7 +457,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 
   case WR_ESCAPEDDOUBLEQUOTE:
     if (record->event.pressed) {
-      SEND_STRING("\\\""SS_TAP(X_ENTER));
+      SEND_STRING("\\\"");
     }
     return false;
     break;
