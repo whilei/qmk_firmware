d388e29c7bf67d467372c3c4b3eb2ac4fc565a69
ia <isaac.ardis@gmail.com>
2020-04-11 10:32:14 -0500

    :floppy_disk:

--- keyboards/ergodox_ez/keymaps/isaac/keymap.c
+++ keyboards/ergodox_ez/keymaps/isaac/keymap.c
@@ -586,16 +586,18 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                  LALT(KC_SCOLON) , CONALT(KC_ENTER) , ___ , LSFT(KC_F2) , KC_F2 , LALT(KC_R) , LCS(KC_F10) , // un-select last occurrence, start new line above, prev error, next error, move line up, run test
                                  LALT(KC_L) , LSFT(KC_ENTER) , LCTL(KC_4) , LSFT(KC_F6) , LSALT(KC_7) , LALT(KC_I) , MEH(KC_F) , // select next highlighted occurrence, start new line,  close tab, rename, find, move line down
                                  LCTL(KC_Y) , LCTL(KC_BSLASH) , ___ , ___ , LCS(KC_N) , LCS(KC_A) , // delete line, expand selection, select thru line start/end, open file, actions
-                                 LCS(KC_9) , LCTL(KC_SLASH) , ___ , LSFT(KC_F7) , KC_F7 , LCS(KC_COMMA) , LALT(KC_1) , // back, comment line, (resolve conflicts)prev,next conflict, clone caret below
+                                 LCS(KC_9) , LCTL(KC_SLASH) , ___ , LSFT(KC_F7) , KC_F7 , LCS(KC_COMMA) , LALT(KC_1) , // back, comment line, (resolve conflicts)prev,next conflict, clone caret below, project view
                                 ___ , ___ , ___ , ___ , ___ ,
                                 ___ , ___ ,
                                 ___ ,
                                 ___ , ___ , ___ ,
                                 // right
-                                LALT(KC_7) , LALT(KC_1) , LALT(KC_2) , LALT(KC_3) , LALT(KC_4) , LALT(KC_5) , LALT(KC_F12) ,
-                                 LALT(KC_HOME) , ___ , ___ , LSFT(KC_F3) , KC_F3 , LCTL(KC_5) , LALT(KC_9) , // prev, next occurrence (word at caret), rerun last, vcs
-                                 ___ , ___ , ___ , ___ , ___ , LCTL(KC_1) , // show error description
-                                 LCS(KC_G)  ,  MEH(KC_PGUP) , ___ , MEH(KC_RBRACKET) , MEH(KC_LBRACKET) , MEH(KC_PGDN) , MEH(KC_Z) , // Toggle minimap
+                                 LALT(KC_7) , LALT(KC_1) , LALT(KC_2) , LALT(KC_3) , LALT(KC_4) , LALT(KC_5) , LALT(KC_9) , // ... , focus git view
+                                 LALT(KC_HOME) , ___ , ___ , LSFT(KC_F3) , KC_F3 , LCTL(KC_5) , LALT(KC_F12) , // focus navigator view, prev / next occurrence (word at caret), rerun last, focus console view
+                                 ___ , ___ ,  MEH(KC_RBRACKET) , MEH(KC_LBRACKET) , ___ , LCTL(KC_1) , // cursor to pane left / right, show error description
+                                 LCS(KC_G)  ,  MEH(KC_PGUP) , ___ , LCA(KC_LBRACKET) , LCA(KC_RBRACKET) , MEH(KC_PGDN) , MEH(KC_Z) , // Toggle minimap, shift pane left, streth pane l/r, shift pan right, toggle distraction free
+
+                                 // LCA(KC_LBRACKET), LCA(KC_RBRACKET) // Stretch panes left / right
 
                                 ___ , ___ , ___ , ___ , ___ ,
                                 ___ , ___ ,
