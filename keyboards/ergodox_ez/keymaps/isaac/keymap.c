cba7cdeb6f827e19ec480d07a9b9519437013a12
ia <isaac.ardis@gmail.com>
2020-04-27 04:03:39 -0500

    :floppy_disk:

--- keyboards/ergodox_ez/keymaps/isaac/keymap.c
+++ keyboards/ergodox_ez/keymaps/isaac/keymap.c
@@ -287,26 +287,27 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                        // Left
                           TD(TD_TAB_TMUXQ) , KC_UP , LT(MACROLAYER, KC_0) , KC_DLR , TG(CAPSLAYER) , KC_DOWN , CTLGUI(KC_K) , // LT( TOPROWALT, KC_TAB )
 
-                          OSM(MOD_LSFT) , LT(FLAYER, KC_Q) , KC_W , KC_D , KC_F , KC_K , MT(MOD_MEH, KC_ENTER ),
+                          ___ , LT(FLAYER, KC_Q) , KC_W , KC_D , KC_F , KC_K , MT(MOD_MEH, KC_ENTER ),
                        LT(SYMBOLS , KC_ESCAPE) , LT(MOTIONLAYER, KC_A) , KC_S , KC_E , KC_T , KC_G ,
-                          KC_LSHIFT ,  LT( QWIMAMU, KC_Z ) , LT(NUMPAD, KC_X) ,  KC_C  , KC_V , LCTL_T( KC_B ) , KC_LEAD , // OSM(MOD_LSFT) , // MO(GOLANDLAYER)
+                          TD(SHIFT_CAP) ,  LT( QWIMAMU, KC_Z ) , LT(NUMPAD, KC_X) ,  KC_C  , KC_V , LCTL_T( KC_B ) , KC_LEAD , // OSM(MOD_LSFT) , // MO(GOLANDLAYER)
 
-                          KC_MS_WH_DOWN,  OSL(UNICODEL) , KC_MS_WH_UP , MT(MOD_LALT, KC_TILD) , MT(MOD_LGUI, KC_QUESTION), // TD(ALT_UNI)
+                          KC_MS_WH_DOWN,  OSL(UNICODEL) , KC_MS_WH_UP , MT(MOD_LALT, KC_SLASH) , MT(MOD_LGUI, KC_SCOLON), // TD(ALT_UNI)
 
                        /*  */
-                          LT(GOLANDLAYER, KC_DELETE) , ___ , // hold for motion layer is nice for left-handed scrolling
+                          LT(GOLANDLAYER, KC_DELETE) , TG(TOPROWNUM) , // hold for motion layer is nice for left-handed scrolling
                        KC_INSERT , // LCTL(KC_TAB) ,
                        SFT_T(KC_SPACE) , KC_BSPACE , KC_DELETE , //  LCS(KC_TAB) , // browser tab left
 
 
                        // Right
-                          CTLGUI(KC_J) , ___ , LSFT(KC_A) , LT(MACROLAYER, KC_MINUS ), KC_UNDS , KC_GRAVE , MO(FLAYER) ,
+                          CTLGUI(KC_J) , ___ , LSFT(KC_A) , LT(MACROLAYER, KC_MINUS ), KC_UNDS , KC_GRAVE , ___ ,
                           /* TD(TD_QUESTION_TOPROWNUM) */
-                          KC_BSPACE , KC_J , KC_U , KC_R , KC_L , LT(FLAYER, KC_SCOLON) , OSL(SYMBOLS) , // OSM(MOD_LSFT) , // LT(DELAYER, KC_QUOTE) , // MT(MOD_HYPR, KC_SCOLON ) 
+                          KC_BSPACE , KC_J , KC_U , KC_R , KC_L , LT(FLAYER, KC_SCOLON) , LSFT(KC_QUOTE) , // OSM(MOD_LSFT) , // LT(DELAYER, KC_QUOTE) , // MT(MOD_HYPR, KC_SCOLON ) 
                           KC_Y , LT(GOLANDLAYER, KC_N ) , KC_I , KC_O ,  KC_H , MO(SYMBOLS), // SYMBOLS_ONE_OR_HOLD, // MO(SYMBOLS), // TD(ONEORMO_SYMBOLS), // MO(SYMBOLS) , // b/c i use symbols a lot, no 200ms wait //
-                          KC_QUESTION , LGUI_T( KC_P ) , KC_M , ALT_T( KC_COMMA ) , KC_DOT , LT(MOTIONLAYER, KC_SLASH ) , KC_RSHIFT, // , TD(SHIFT_CAP), // OSM(MOD_LSFT) , // KC_RSHIFT ,
+                          KC_QUESTION , LGUI_T( KC_P ) , KC_M , ALT_T( KC_COMMA ) , KC_DOT , LT(MOTIONLAYER, KC_SLASH ) , TD(SHIFT_CAP) , // , TD(SHIFT_CAP), // OSM(MOD_LSFT) , // KC_RSHIFT ,
 
-                          MT(MOD_LCTL, KC_QUOTE) , MT(MOD_LALT, KC_SCOLON) , TG(NUMPAD) , TG(MOTIONLAYER), CONALT(KC_0) , // mute/unmute microphone
+                          MT(MOD_LCTL, KC_COMMA) , MT(MOD_LALT, KC_DOT) , TG(NUMPAD) , TG(MOTIONLAYER), CONALT(KC_0) , // mute/unmute microphone
+                          /* MT(MOD_LCTL, KC_QUOTE) , MT(MOD_LALT, KC_SCOLON) , TG(NUMPAD) , TG(MOTIONLAYER), CONALT(KC_0) , // mute/unmute microphone */
 
                        LGUI(KC_H) , LGUI(KC_L) ,
                        LGUI(KC_RIGHT) ,
@@ -465,9 +466,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                       ___ , KC_AT , KC_DLR , KC_LEFT_PAREN , KC_RIGHT_PAREN, KC_KP_PLUS ,
                       ___ , KC_TILD , KC_GRAVE , KC_LBRACKET, KC_RBRACKET , KC_HASH , ___ ,
                        ___ , ___ , ___ , ___ , ___ ,
-                       ___ , ___ ,
+
+                      KC_COMMA,  KC_DOT,
                        ___ ,
                        ___ , ___ , ___ ,
+
                        // right
                       ___ , KC_6 , KC_7 , KC_8 , KC_9 , KC_0 , ___ ,
                       /* ___ , KC_5 , KC_6 , KC_7 , KC_8 , KC_9 , ___ , */
@@ -477,7 +480,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                       ___ , KC_KP_ASTERISK , KC_UNDS , KC_LABK , KC_RABK , ___ , ___ , //
                                               ___ , ___ , ___ , ___ , ___ ,
 
-                       ___ , ___ ,
+                      KC_COMMA,  KC_DOT,
                        ___ ,
                        ___ , ___ , ___
                        ),
@@ -1530,7 +1533,8 @@ uint32_t layer_state_set_user(uint32_t state) {
  */
 int cur_dance (qk_tap_dance_state_t *state) {
   if (state->count == 1) {
-    if (state->interrupted || !state->pressed)  return SINGLE_TAP;
+    /* if (state->interrupted || !state->pressed)  return SINGLE_TAP; */
+    if (!state->pressed)  return SINGLE_TAP;
     //key has not been interrupted, but they key is still held. Means you want to send a 'HOLD'.
     else return SINGLE_HOLD;
   }
@@ -1604,6 +1608,7 @@ void shift_cap_finished (qk_tap_dance_state_t *state, void *user_data) {
       layer_on(CAPSLAYER);
     }
     break;
+  case DOUBLE_HOLD: register_code(KC_LSHIFT); break;
   }
 };
 
