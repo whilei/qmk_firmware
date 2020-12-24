e6d37fb0b3f54a0fb8008cc4a37ad84bcd8473ab
ia <b5c6@protonmail.com>
2020-12-24 12:48:40 -0600

    :floppy_disk:

--- keyboards/ergodox_ez/keymaps/isaac/keymap.c
+++ keyboards/ergodox_ez/keymaps/isaac/keymap.c
@@ -348,7 +348,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_SPACE , ___ , ___ ,
 
   // right hand
-  ___,      KC_6,    KC_7,    KC_8,    KC_9,              KC_0,           ___,
+  ___,      KC_6,    KC_7,    KC_8,    KC_9,              KC_0,           KC_ESC,
   ___,     KC_Y,    KC_U,    KC_I,    KC_O,              KC_P,           ___,
   KC_H,         KC_J,    KC_K,    KC_L,    KC_SCLN, ___,
   ___, KC_N,    KC_M,    KC_COMM, KC_DOT,            ___, ___,
