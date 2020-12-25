5e902b7c213672653e0381339b7b3988a8bbc539
ia <b5c6@protonmail.com>
2020-12-25 05:50:33 -0600

    :hammer:

--- keyboards/ergodox_ez/keymaps/isaac/keymap.c
+++ keyboards/ergodox_ez/keymaps/isaac/keymap.c
@@ -337,18 +337,18 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                           ),
 
   [DEADQWERTY] = LAYOUT_ergodox(
-  KC_F1,          KC_1,        KC_2,          KC_3,    KC_4,    KC_5,    ___,
+  KC_F1,          KC_1,        KC_2,          KC_3,    KC_4,    KC_5,    KC_ESC,
   KC_TAB,          KC_Q,        KC_W,          KC_E,    KC_R,    KC_T,    ___,
   KC_CAPS,         KC_A,        KC_S,          KC_D,    KC_F,    KC_G,
   KC_LSHIFT,         CTL_T(KC_Z), KC_X,          KC_C,    KC_V,    KC_B,    KC_LCTL ,
-  KC_LCTL, ___ , ___ , ___ , ___ ,
+  KC_LCTL, ___ , ___ , KC_LCTL , KC_TAB ,
 
   KC_LBRACKET , KC_RBRACKET ,
   ___ ,
-  KC_SPACE , ___ , ___ ,
+  KC_SPACE , ___ , KC_M ,
 
   // right hand
-  ___,      KC_6,    KC_7,    KC_8,    KC_9,              KC_0,           KC_ESC,
+  ___,      KC_6,    KC_7,    KC_8,    KC_9,              KC_0,           ___ ,
   ___,     KC_Y,    KC_U,    KC_I,    KC_O,              KC_P,           ___,
   KC_H,         KC_J,    KC_K,    KC_L,    KC_SCLN, ___,
   ___, KC_N,    KC_M,    KC_COMM, KC_DOT,            ___, ___,
@@ -356,7 +356,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   ___ , ___ ,
   ___ ,
-  ___ , ___ , ___
+  ___ , ___ , KC_ENTER
                                 ),
 
   [CAPSLAYER] = LAYOUT_ergodox(
